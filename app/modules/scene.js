329fd47bc5c4e2e81a50f5e5b0b4e850c91bf341
Daisy Smith <daisymarie128@gmail.com>
2017-03-01 16:32:44 +0000

    create scene

---
+++ app/modules/scene.js
@@ -0,0 +1,36 @@
+import {
+	PerspectiveCamera,
+	Scene,
+	WebGLRenderer,
+} from 'three';
+
+export default class SceneHolder {
+	init() {
+		const container = document.getElementById('container');
+		this.camera = new PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
+		this.camera.position.z = 400;
+		this.scene = new Scene();
+		this.renderer = new WebGLRenderer();
+		this.renderer.setPixelRatio(window.devicePixelRatio);
+		this.renderer.setSize(window.innerWidth, window.innerHeight);
+		// this.renderer.setClearColor(0xffffff);
+
+		container.appendChild(this.renderer.domElement);
+	}
+
+	static camera() {
+		return this.camera;
+	}
+
+	static scene() {
+		return this.scene;
+	}
+
+	static renderer() {
+		return this.renderer;
+	}
+
+	start() {
+		return this.init();
+	}
+}
