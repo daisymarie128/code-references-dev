b8fe2bc4846c8f08ed8e00f29177114fb030ff00
Daisy Smith <daisymarie128@gmail.com>
2017-03-01 17:36:12 +0000

    add progress bar plus copy assets folder dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
-var HTMLWebpackPlugin = require('html-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
 	entry: {
-		'index': ['./app/index.js', './app/main.scss'],
+		'index': [
+			'./app/index.js', './app/main.scss'
+		],
 		'materials': ['./app/examples/materials/materials.js', './app/examples/materials/materials.scss']
 	},
 	output: {
@@ -10,28 +14,34 @@ module.exports = {
 		filename: 'js/[name].js'
 	},
 	module: {
-			rules: [
-				{
-     test: /\.js$/,
-     exclude: /node_modules/,
-     loader: 'babel-loader',
-    },
-				{
-						test: /\.scss$/,
-						use: ['style-loader', 'css-loader', 'sass-loader'],
-				}
-			]
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader'
+			}, {
+				test: /\.scss$/,
+				use: ['style-loader', 'css-loader', 'sass-loader']
+			}, {
+				test: /\.jpe?g$|\.gif$|\.png$/i,
+				loader: 'file-loader?regExp=(image.*)&name=[1]'
+			}
+		]
 	},
 	plugins: [
+		new ProgressBarPlugin(),
+		new CopyWebpackPlugin([
+			{ from: './app/assets', to: 'assets/' }
+		]),
 		new HTMLWebpackPlugin({
 			inject: 'body',
-			template: __dirname + '/app/index.html',
+			template: `${__dirname}/app/index.html`,
 			filename: 'index.html',
 			chunks: ['index']
 		}),
 		new HTMLWebpackPlugin({
 			inject: 'body',
-			template: __dirname + '/app/examples/materials/materials.html',
+			template: `${__dirname}/app/examples/materials/materials.html`,
 			filename: 'examples/materials/index.html',
 			chunks: ['materials']
 		})
